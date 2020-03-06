98f16c582d74d3e44a44138545e356998de2ac76
esemboloni <esemboloni@schubergphilis.com>
2020-03-06 16:22:53 +0100

    Bumped version and changed to use jks (only partially)

---
+++ src/main/java/com/example/demo/Saml2Properties.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Data
+@Configuration
+@ConfigurationProperties("app.security.saml2")
+public class Saml2Properties {
+
+    private IdentityProvider identityProvider;
+    private Signing signing;
+
+
+    @Data
+    public static class IdentityProvider {
+        private String entityId;
+        private String ssoUrl;
+        private Verification verification;
+
+
+        @Data
+        public static class Verification {
+            private Resource keyStoreLocation;
+            private String keyStorePassword;
+            private String keyStoreAlias;
+        }
+    }
+
+    @Data
+    public static class Signing {
+        private Resource privateKeyLocation;
+        private Resource certificateLocation;
+    }
+}
+
